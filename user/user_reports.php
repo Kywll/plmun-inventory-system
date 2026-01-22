2058fdc30c5557327702876defe4d13cff6958df
Kywll <mkywll74@gmail.com>
2026-01-22 14:15:20 +0800

    Add user and admin pages php files

---
+++ user/user_reports.php
@@ -0,0 +1,8 @@
+<?php
+    include("includes/db_connect.php");
+    session_start();
+
+    include_once('includes/header.php');
+    include_once('includes/footer.php');
+    include_once('includes/sidebar_user.php');
+?>
